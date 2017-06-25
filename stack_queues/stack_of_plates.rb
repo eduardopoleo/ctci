e4c6d74bf57a4a8986e7473f74a201a448af8b92
Eduardo Poleo <dev+eduardo@influitive.com>
2017-06-25 12:20:01 -0400

    Stack of plates shift left

--- stack_queues/stack_of_plates.rb
+++ stack_queues/stack_of_plates.rb
@@ -29,6 +29,27 @@ class Stack
     old_top.next = nil
   end
 
+  def shift
+    current = @top
+
+    if current.next == nil
+      return current
+    end
+
+    while true
+      next_node = current.next
+
+      if next_node.next == nil
+        current.next = nil
+        break
+      end
+
+      current = next_node
+    end
+
+    next_node
+  end
+
   def full?
     size == max_size
   end
@@ -66,28 +87,62 @@ class SetOfStacks
     end
   end
 
+  def pop_at(index)
+    if index == current_index
+      self.pop
+    else
+      stacks[index].pop
+      shift_left(index)
+    end
+  end
+
   private
 
   def current_stack
     current_stack = stacks[current_index]
   end
+
+  def shift_left(index)
+    current_stack = stacks[index]
+    next_stack = stacks[index + 1]
+
+    bottom_node = next_stack.shift
+    current_stack.push(bottom_node)
+
+    if index < @stacks.size - 2
+      shift_left(index + 1)
+    end
+  end
 end
 
 
 node1 = Node.new(1)
-stack1 = Stack.new(node1, 2)
-set = SetOfStacks.new(stack1, 2)
-
 node2 = Node.new(2)
 node3 = Node.new(3)
+
 node4 = Node.new(4)
+node5 = Node.new(5)
+node6 = Node.new(6)
+
+node7 = Node.new(7)
+node8 = Node.new(8)
+node9 = Node.new(9)
+
+stack1 = Stack.new(node1, 3)
+set = SetOfStacks.new(stack1, 3)
 
 set.push(node2)
 set.push(node3)
+
 set.push(node4)
+set.push(node5)
+set.push(node6)
 
-set.pop
-set.pop
-set.pop
+set.push(node7)
+set.push(node8)
+set.push(node9)
 
-p set.stacks
+set.pop_at(2)
+
+p set.stacks[1]
+p set.stacks[2]

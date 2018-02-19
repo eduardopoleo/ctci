f4b4d6bd6c441359281337e9a1381f3a6c40a30c
Eduardo Poleo <dev+eduardo@influitive.com>
2018-02-19 09:07:32 -0500

    All solutions

--- trees_graphs/questions/is_binary_search_tree.rb
+++ trees_graphs/questions/is_binary_search_tree.rb
@@ -34,25 +34,55 @@ p ordered?(list)
 
 ## Approach 2 without creating the array checking on the fly from the last
 # visited node
-$last_seen = nil
 def in_order_search2(node)
   return true if node == nil
 
-  in_order_search2(node.left)
+  return false if !in_order_search2(node.left)
 
   if $last_seen != nil && ($last_seen > node.value)
     return false
   end
-
+  # it has to be global otherwise we can't keep track of the last_seen
+  # value when we start backtracking.
   $last_seen = node.value
 
-  in_order_search2(node.right)
+  return false if !in_order_search2(node.right) 
 
   return true
 end
 
 puts '## Not Binary. last seen should always be smaller  ##'
+$last_seen = nil
 p in_order_search2(root_not_binary)
 
 puts '## Binary. last seen should always be smaller  ##'
-p in_order_search2(root_not_binary)
+$last_seen = nil
+p in_order_search2(root_binary)
+
+## Aproach 3 using the binary search tre definition min/max
+# n.left.value < n.value < n.right.value
+
+def in_order_search3(node, max, min)
+  return true if node == nil
+
+  if max && (node.value > max)
+    return false
+  end
+
+  if min && (node.value < min)
+    return false
+  end
+
+  if !in_order_search3(node.left, node.value, min) ||
+      !in_order_search3(node.right, max, node.value)
+    return false
+  end
+
+  return true
+end
+
+puts '## Not Binary. min max  ##'
+p in_order_search3(root_not_binary, nil, nil)
+
+puts '## Not Binary. min max  ##'
+p in_order_search3(root_binary, nil, nil)
